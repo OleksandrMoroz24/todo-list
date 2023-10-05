c515a3bca70b1bc0a0998e4d1a369e82a01d7f93
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-10-05 11:35:59 +0300

    make views and blank templates

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from todo.models import Tag, Task
+from todo.forms import TaskForm, TagForm
+
+
+class TaskListView(ListView):
+    model = Task
+    form = TaskForm
+    template_name = "todo/home.html"
+    paginate_by = 5
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:home")
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:home")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "todo/task_confirm_delete.html"
+    success_url = reverse_lazy("todo:home")
+
+
+class TagListView(ListView):
+    model = Tag
+    form = TagForm
+    template_name = "todo/tag_list.html"
+    paginate_by = 5
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    form = TagForm
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag_list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    form = TagForm
+    template_name = "todo/tag_form.html"
+    success_url = reverse_lazy("todo:tag_list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = "todo/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo:tag_list")
+
