ab1d4e5d584ad48d8df7a918acf7f9c64f9a4d3b
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-10-05 11:36:31 +0300

    update settings and urls

---
+++ todo/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
+
+
+urlpatterns = [
+    path("", TaskListView.as_view, name="home"),
+    path("task/add/", TaskCreateView.as_view(), name='task-add'),
+    path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task-edit'),
+    path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+
+    path("tags/", TagListView.as_view, name="tags-list"),
+    path("tags/add/", TagCreateView.as_view(), name='tags-add'),
+    path('tags/<int:pk>/edit/', TagUpdateView.as_view(), name='tags-edit'),
+    path('tags/<int:pk>/delete/', TagDeleteView.as_view(), name='tags-delete'),
+
+]
