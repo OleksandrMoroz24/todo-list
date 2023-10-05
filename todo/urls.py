28c64d202d6799d048e5b47b2659da766c2b2b35
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-10-05 13:44:02 +0300

    make change status button

--- todo/urls.py
+++ todo/urls.py
@@ -8,21 +8,22 @@ from todo.views import (
     TagListView,
     TagCreateView,
     TagUpdateView,
-    TagDeleteView
+    TagDeleteView,
+    toggle_status
 )
 
 
 urlpatterns = [
-    path("", TaskListView.as_view, name="home"),
+    path("", TaskListView.as_view(), name="home"),
     path("task/add/", TaskCreateView.as_view(), name='task-add'),
     path('task/<int:pk>/edit/', TaskUpdateView.as_view(), name='task-edit'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
 
-    path("tags/", TagListView.as_view, name="tags-list"),
+    path("tags/", TagListView.as_view(), name="tags-list"),
     path("tags/add/", TagCreateView.as_view(), name='tags-add'),
     path('tags/<int:pk>/edit/', TagUpdateView.as_view(), name='tags-edit'),
     path('tags/<int:pk>/delete/', TagDeleteView.as_view(), name='tags-delete'),
-
+    path('task/toggle_status/<int:task_id>/', toggle_status, name='toggle_status'),
 ]
 
 app_name = "todo"
